89d4869495d7d39256aea684343803a2ed1b7fe9
lujiyuan <1309583224@qq.com>
2019-05-07 14:52:16 +0800

    提交数据

--- js/rem.js
+++ js/rem.js
@@ -3,8 +3,8 @@
         var html = document.documentElement;
         var w = html.getBoundingClientRect().width;
         // 如果html宽度大于750，按照750的原稿尺寸显示
-        w = w>750?750:w;
-        var fontSize = w/7.5;   //7.5指的是设计稿的尺寸为750，如果设计稿的尺寸为828，那么应该是w/8.28
+        w = w>640?640:w;
+        var fontSize = w/6.4;   //7.5指的是设计稿的尺寸为750，如果设计稿的尺寸为828，那么应该是w/8.28
         html.style.fontSize = fontSize+"px";
     }
     resize();
